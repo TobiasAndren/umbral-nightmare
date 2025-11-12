27ad154d4c53eae79a60c576ae08e9e65785eb14
TobiasAndren <tobiasandren@hotmail.com>
2025-11-12 11:49:51 +0100

    Fix: temporary namechange to fix vercel error

--- src/scenes/DeathMenuScene.ts
+++ src/scenes/DeathMenuScene.ts
@@ -6,7 +6,7 @@ import {
 import { UIButton } from "../ui/UIButton";
 import type { GameAudio } from "../helpers/gameAudio/GameAudio";
 import { getGameAudio } from "../helpers/gameAudio/gameAudioManager";
-import { GameState } from "../helpers/temp";
+import { GameState } from "../helpers/gameState";
 
 export class DeathMenuScene extends Phaser.Scene {
   private audio?: GameAudio;
